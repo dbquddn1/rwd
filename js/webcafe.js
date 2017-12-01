0f189976186adb969904a5e9e0445477aa59cbee
dbquddn1 <dbquddn1@gmail.com>
2017-12-01 17:30:46 +0900

    수정파일 및 북마크 문서

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
   var container = $('.menubar-container');
+  var btn = $('.btn-menubar');
   var menubar = $('.btn-menubar, .menubar');
   var menu = $('.main-menu');
-  var btn = $('.btn-menubar');
   var subMenu = $('.sub-menu');
   var li = $('.main-menu > li');
   var last = $('.main-menu > li:last-child');
@@ -12,42 +12,34 @@ $(document).ready(function() {
     container.toggleClass('menubar-act');
     menu.toggleClass('menu-act');
     if (container.hasClass('menubar-act')) {
-      btn.text('메인메뉴 열기');
+      btn.text('메인 메뉴 닫기');
     } else {
-      btn.text('메인메뉴 닫기');
+      btn.text('메인 메뉴 열기');
     }
   });
-
   li.click(function(e) {
     e.preventDefault();
     $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
     $(this).find('.sub-menu').addClass('sub-menu-act');
   });
-
   li.keyup(function(e) {
     e.preventDefault();
     if (e.keyCode == 13 || e.keyCode == 32) {
       $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
       $(this).find('.sub-menu').addClass('sub-menu-act');
     }
-
   });
-
   li.focusin(function() {
     $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
   });
-
   last.focusout(function() {
-    if (!subMenu.hasClass('sub-menu-act')) {
-      menu.removeClass('menu-act');
+    if (!(subMenu.hasClass('sub-menu-act'))) {
       container.removeClass('menubar-act');
+      menu.removeClass('menu-act');
     }
   });
-
   subLast.focusout(function() {
-    menu.removeClass('menu-act');
     container.removeClass('menubar-act');
+    menu.removeClass('menu-act');
   });
-
-
 });
