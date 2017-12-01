caaa473ec328cd1449893ebbeb27d7f4fb64b7b3
dbquddn1 <dbquddn1@gmail.com>
2017-12-01 16:52:52 +0900

    반응형웹 실습 예제

---
+++ js/webcafe.js
@@ -0,0 +1,53 @@
+$(document).ready(function() {
+  var container = $('.menubar-container');
+  var menubar = $('.btn-menubar, .menubar');
+  var menu = $('.main-menu');
+  var btn = $('.btn-menubar');
+  var subMenu = $('.sub-menu');
+  var li = $('.main-menu > li');
+  var last = $('.main-menu > li:last-child');
+  var subLast = $('.main-menu li:last-child li:last-child a');
+
+  menubar.click(function() {
+    container.toggleClass('menubar-act');
+    menu.toggleClass('menu-act');
+    if (container.hasClass('menubar-act')) {
+      btn.text('메인메뉴 열기');
+    } else {
+      btn.text('메인메뉴 닫기');
+    }
+  });
+
+  li.click(function(e) {
+    e.preventDefault();
+    $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
+    $(this).find('.sub-menu').addClass('sub-menu-act');
+  });
+
+  li.keyup(function(e) {
+    e.preventDefault();
+    if (e.keyCode == 13 || e.keyCode == 32) {
+      $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
+      $(this).find('.sub-menu').addClass('sub-menu-act');
+    }
+
+  });
+
+  li.focusin(function() {
+    $(this).siblings().find('.sub-menu').removeClass('sub-menu-act');
+  });
+
+  last.focusout(function() {
+    if (!subMenu.hasClass('sub-menu-act')) {
+      menu.removeClass('menu-act');
+      container.removeClass('menubar-act');
+    }
+  });
+
+  subLast.focusout(function() {
+    menu.removeClass('menu-act');
+    container.removeClass('menubar-act');
+  });
+
+
+});
